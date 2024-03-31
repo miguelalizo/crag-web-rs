3b1bb32a3a687d2c004cdd387f5faa63288d0d48
Miguel Alizo <miguelalizo@Ariannas-MacBook-Air.local>
2024-03-31 15:08:01 -0700

    Delete unwanted files

--- tcp-server/target/debug/deps/tcp_server-2728690111cc8bf5.d
+++
@@ -1,5 +0,0 @@
-/Users/miguelalizo/projects/portfolio-rust-server/rust-server/tcp-server/target/debug/deps/tcp_server-2728690111cc8bf5: src/main.rs
-
-/Users/miguelalizo/projects/portfolio-rust-server/rust-server/tcp-server/target/debug/deps/tcp_server-2728690111cc8bf5.d: src/main.rs
-
-src/main.rs:
--- tcp-server/target/debug/tcp-server.d
+++
@@ -1,1 +0,0 @@
-/Users/miguelalizo/projects/portfolio-rust-server/rust-server/tcp-server/target/debug/tcp-server: /Users/miguelalizo/projects/portfolio-rust-server/rust-server/tcp-server/src/main.rs
