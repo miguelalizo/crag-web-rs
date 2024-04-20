a730bee8eec51cd591e5ec827319057fb11e0b25
miguelalizo <miguel.e.alizo@gmail.com>
2024-04-20 13:56:17 -0700

    Add default_error_404_handler to handlers.rs

--- tcp-server/src/handler.rs
+++ tcp-server/src/handler.rs
@@ -1,3 +1,20 @@
 use crate::response;
 
 pub type Handler = fn() -> response::Response;
+
+/// Default handler for 404 errors
+pub fn default_error_404_handler() -> response::Response {
+    let bytes = include_bytes!("../static/html/404.html");
+    let status_line = "HTTP/1.1 404 Not Found";
+    let len = bytes.len();
+
+    // format http response
+    let response = format!(
+        "{status_line}\r\nContent-Type: text/html\r\nContent-Length: {len}\r\n\r\n"
+    );
+
+    let mut full_response = response.into_bytes();
+    full_response.extend(bytes);
+
+    response::Response { content: full_response }
+}
