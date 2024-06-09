e7b6c1aafae104387deb1319dff4566ceeffe084
miguelalizo <miguel.e.alizo@gmail.com>
2024-06-09 07:00:14 +0000

    Update hello test

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,4 @@
-use crag_web::{request, response, server};
+use crag_web::{request, response, routes, server};
 
 // get "/hello"
 fn hello_handler(_request: request::Request) -> anyhow::Result<response::Response> {
@@ -17,7 +17,7 @@ fn main() -> anyhow::Result<()> {
     let app = server::Server::build()
         .register_error_handler(Box::new(error_404_handler))?
         .register_handler(
-            request::Request::GET(String::from("/hello")),
+            routes::Route::new(String::from("/hello")),
             Box::new(hello_handler),
         )?
         .finalize(("127.0.0.1", 8010), 4)
