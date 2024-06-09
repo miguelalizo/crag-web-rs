e657c267d78dd30e3ba9707e95351ed8d1410098
miguelalizo <miguel.e.alizo@gmail.com>
2024-06-09 06:56:18 +0000

    Add Route and Route::new() + test

--- crag-web/src/routes.rs
+++ crag-web/src/routes.rs
@@ -2,3 +2,22 @@
 pub struct Route {
     pub route: String,
 }
+
+impl Route {
+    pub fn new(route: impl AsRef<str>) -> Self {
+        Route {
+            route: route.as_ref().to_owned(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_route_new() {
+        let route = Route::new("/foo");
+        assert_eq!(route.route, "/foo".to_owned());
+    }
+}
